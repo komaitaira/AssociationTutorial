15d99ad8b8f2f4bc6c664ce03c84aad65ff714c8
komaitaira <komaitaira@gmail.com>
2020-05-27 19:47:55 +0900

    userとtweetモデル間のアソシエーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,9 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    # ===============追加==============
+    @tweets = @user.tweets #@user.tweetsは複数のツイートが入った配列となっております。
+    # ================================
   end
 
 end
